24b4b1a028435e76331971cedd544c48d1b72b72
Nicolas Rey <nicolasreydh@gmail.com>
2020-09-17 18:17:56 -0500

    Updates

---
+++ precache-manifest.390fbe36ae6df55e3c30f61a88f49f27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84699a1e87574f8c23ef396ae99c4594",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c3c4c05d9ab3bc4e4f32",
+    "url": "/monsters-rolodex/static/css/main.05cbfb98.chunk.css"
+  },
+  {
+    "revision": "e71eaf6f477149fa2654",
+    "url": "/monsters-rolodex/static/js/2.3d55fb81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.3d55fb81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3c4c05d9ab3bc4e4f32",
+    "url": "/monsters-rolodex/static/js/main.acf68e52.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
